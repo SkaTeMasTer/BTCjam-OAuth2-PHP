7a618eb89481b5d7bf1f28b45b91f23cbfe10eb9
Shawn Reimerdes <shawn.reimerdes@gmail.com>
2015-12-22 18:09:56 -0500

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -5,23 +5,41 @@
 // | + SOURCE CODE: https://github.com/SkaTeMasTer/BTCjam-OAuth2-PHP/
 // | + Written by: Shawn Reimerdes (Leech Software)
 // ------------------------------------------------------------------------------------
+
 // debug
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
 session_start();
 
 require_once('config.php');
 require_once('http_client.class.php');
 
-$accessToken = $_SESSION['access_token'];
+if (isset($_SESSION) && !empty($_SESSION['access_token'])) {
 
-// *** The OAuth2 dance is complete.  We can now make calls to the API.
+        	$accessToken = $_SESSION['access_token'];
+
+} else {
+
+			 /* no valid access token available, go to authorization server */
+				   
+			 //header('HTTP/1.1 302 Found');
+			 header('Location: '. $apiConfig['authorizationUrlBase']);
+			 exit;
+				//	die("<p>Sorry, no access token found or our SESSION has expired.</p>\n");
+
+}
+
+
+
 
 
 // OAuth2 authorized requests must send the 'access_token' via a 
 // modified HTTP header w/ Authorization Bearer Token. The protocol
 // outlines this security method instead of a QUERY POST, 
 // in order to protect our tokens from plain-text and browsers 
+
+// *** The OAuth2 dance is complete.  We can now make calls to the API.
 
 // -------------------------------------------------------------------------------------------
 
@@ -39,21 +57,29 @@ $accessToken = $_SESSION['access_token'];
 	
 	// catch () {}	
 
- 	
-
-// todo: obviously this is just a demo, bad coding here... needs to be seperated into a new class, etc...
-
-
-// there is a single method in the API implemented here...
+ 	//var_dump($responseArray);
 
 
 	// *** 'User Profile' Endpoint 
 
 
+	echo "<p>[*] 'User Profile' Endpoint / using Scope(s): basic_profile, extended_profile ...</p>" . PHP_EOL;
+	
 
-	echo "<p>[*] 'User Profile' Endpoint / using Scope(s): basic_profile, extended_profile ...</p>\n";
-	print_r($responseArray);
+	
+?>
+<hr>
 
+<h1>Hi, <?=$responseArray['user']['name'];?></h1>
+<img src='<?=$responseArray['user']['avatar_url'];?>' width="75px" height="75px" />
+
+<p>You are currently logged on.</p>
+
+<p>Your Balance = <?=$responseArray['user']['balance'];?> BTC</p>
+
+<hr>
+
+<?
 
 
 
