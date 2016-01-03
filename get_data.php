cd55b36fc56ab8cd3e1a2bf3669268c5e78ae990
Shawn Reimerdes <shawn.reimerdes@gmail.com>
2016-01-03 14:01:07 -0500

    All the action happens here...
    added SESSION caching

--- get_data.php
+++ get_data.php
@@ -7,38 +7,59 @@
 // ------------------------------------------------------------------------------------
 
 // debug
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 1); error_reporting(E_ALL);
+
+// todo: obviously this is just a demo, bad coding here... needs to be seperated into a new class, etc...
+// there is just a single API call in the demo here...
+
 
 session_start();
 
 require_once('config.php');
 require_once('http_client.class.php');
 
+
+$cache_filename = '/tmp/php_session_btcjam.txt'; // ensure this file/dir has write permissions!!!
+
 if (isset($_SESSION) && !empty($_SESSION['access_token'])) {
 
-        	$accessToken = $_SESSION['access_token'];
 
+        			  $accessToken = $_SESSION['access_token'];
+        	
+        	 		  // *** new session started -- cache session to local file (prevents needing to reauthorize your app)
+
+					  echo "<p>[*] Creating file to cache access_token (" . $cache_filename . ") ...</p>" . PHP_EOL;
+						   
+				      $session_data = session_encode(); 
+				      $fh = fopen ($cache_filename, 'w+'); 		
+				      fwrite ($fh, $session_data); 
+				      fclose ($fh);
+			
 } else {
-			 /* no valid access token available, go to authorization server */
-				   
-			 // header('HTTP/1.1 302 Found');
-			 // header('Location: '. $apiConfig['authorizationUrlBase']);
-			 // exit;
-			 
-			die("<p>Sorry, no access token found or our SESSION has expired.</p>\n");
+
+					  // *** session has expired -- load our cached access_token from a local file 
+
+					  echo "<p>[*] Loading access_token from cached file ...</p>" . PHP_EOL;
+						   
+			      	  $fh = fopen ($cache_filename, 'r');
+				      $sessiondata = fread ($fh, 4096); 
+				      fclose ($fh);
+				      session_decode($sessiondata); 
+
+        			  $accessToken = $_SESSION['access_token'];
+        			  
+		 			 // header('HTTP/1.1 302 Found');
+					 // header('Location: '. $apiConfig['authorizationUrlBase']);
+					 // exit;
+
+					 if (empty($accessToken)) die("<p>Sorry, no access token found or our SESSION has expired.</p>" . PHP_EOL);       			  
 }
 
 
 
 
 
-// OAuth2 authorized requests must send the 'access_token' via a 
-// modified HTTP header w/ Authorization Bearer Token. The protocol
-// outlines this security method instead of a QUERY POST, 
-// in order to protect our tokens from plain-text and browsers 
 
-// *** The OAuth2 dance is complete.  We can now make calls to the API.
 
 // -------------------------------------------------------------------------------------------
 
@@ -61,26 +82,26 @@ if (isset($_SESSION) && !empty($_SESSION['access_token'])) {
 
 	// *** 'User Profile' Endpoint 
 
-
-	echo "<p>[*] 'User Profile' Endpoint / using Scope(s): basic_profile, extended_profile ...</p>" . PHP_EOL;
-	
-
-	
+	echo "<p><hr>[*] 'User Profile' Endpoint:</p>" . PHP_EOL;
+		
 ?>
-<hr>
+
 
 <h1>Hi, <?=$responseArray['user']['name'];?></h1>
 <img src='<?=$responseArray['user']['avatar_url'];?>' width="75px" height="75px" />
 
-<p>You are currently logged on.</p>
+<ul>
+		<li>Email: <?=$responseArray['user']['email'];?></li>
 
-<p>Your Balance = <?=$responseArray['user']['balance'];?> BTC</p>
+		<li>BTCjam Score: <?=$responseArray['user']['btcjam_score'];?> (<?=$responseArray['user']['btcjam_score_numeric'];?>)</li>
 
-<hr>
+		<li>Your Balance = <?=$responseArray['user']['balance'];?> BTC</li>
+</ul>
+---------------------------------------
 
 <?
 
-
+// -------------------------------------------------------------------------------------------
 
 
 ?>
